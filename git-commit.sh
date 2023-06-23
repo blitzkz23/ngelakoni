5a86b4de68ceab4fc732717bc62ed92c882bc0f2
blitzkz23 <blitzaldykazu@gmail.com>
2023-06-24 06:20:21 +0700

    feat:modify sh command with new args

--- git-commit.sh
+++ git-commit.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 commit_message="$1"
+origin="$2"
 
 git add .
 git commit -m "$commit_message"
-git push origin main
+git push origin "$origin"
 
 # how to run :
 # ./git-commit.sh "enter message"
